e7d7db42187fb37ca167f1104304986ea469982f
hyeyeon-sun <dlgpdus0416@naver.com>
2021-05-04 14:52:49 +0900

    에러메세지 출력, 폴더 앱 생성, 폴더 앱과 로그인 페이지 연동

--- Khurm/urls.py
+++ Khurm/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('user/', include('user.urls')),
+    path('folder/', include('folder.urls')),
     path('admin/', admin.site.urls),
 ]
--- user/views.py
+++ user/views.py
@@ -1,28 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import auth
 
 def signup(request):
     if request.method == "POST":
-        if request.POST["password1"] == request.POST["password1"]:
+        if request.POST["password1"] == request.POST["password2"]:
             user = User.objects.create_user(
                 username = request.POST["username"], password = request.POST["password1"])
 
             auth.login(request, user)
-            return render(request, 'main.html')
-        return render(request, 'signup.html', {'error': '패스워드 확인이 일치하지 않습니다'})
-
+            return redirect('../../folder/')
+        return render(request, 'signup.html', {'error': 'password does not match'})
     return render(request,'signup.html')
 
 def login(request):
+     
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
         user = auth.authenticate(request, username = username, password = password)
         if user is not None:
             auth.login(request, user)
-            return render(request, 'main.html')
+            return redirect('../../folder/')
         else:
             return render(request, 'login.html', {'error': 'user name or password is incorrect'})
 
@@ -31,3 +31,4 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return render('login.html')
+
