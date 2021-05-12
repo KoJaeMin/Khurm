799e5287c0f92391e1dd81c6eb21b0e7ecfee58f
KimJyun <jhkim311@naver.com>
2021-05-12 16:41:44 +0900

    수정할거임. 무시해도됨.

--- Khurm/settings.py
+++ Khurm/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'Khurm.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'khurm',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
