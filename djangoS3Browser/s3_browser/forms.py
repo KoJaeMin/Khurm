163fe06c6a836ee91c2e696103a030e9bb53ca74
hyeyeon-sun <dlgpdus0416@naver.com>
2021-06-01 15:48:17 +0900

    얼굴 인식 api 연동

--- djangoS3Browser/s3_browser/forms.py
+++ djangoS3Browser/s3_browser/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from folder.models import File
+import json, requests
+
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = File
         fields = '__all__'
+
+
