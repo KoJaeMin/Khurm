02f21495356049e0afb46433c627c71cf218e275
KimJyun <jhkim311@naver.com>
2021-06-01 20:26:45 +0900

    즐겨찾기 api 추가

--- folder/urls.py
+++ folder/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from django.contrib import admin
 from django.conf.urls import include
 from rest_framework.routers import SimpleRouter
-from .views import FileViewset, SharedViewset
+from .views import FileViewset, SharedViewset, FavoriteViewset
 
 router = SimpleRouter()
 router.register('accounts', FileViewset)
 router.register('shared', SharedViewset)
+router.register('favorite', FavoriteViewset)
 # router는 url패턴에 추가하는 방식과 다르게 마지막에 / 입력하면 오류
 
 urlpatterns = router.urls
